3957bd801c8c5365d1e584a719bb5622b05207c8
artiya <artiya@siamsquared.com>
2015-07-25 11:06:01 +0700

    Add HappyMan class.

---
+++ src/main/java/me/artiya/jiradt4j/HappyMan.java
@@ -0,0 +1,50 @@
+package me.artiya.jiradt4j;
+
+/**
+ * เราต่างก็ดิ้นรนต่อสู้เรื่องต่างๆ
+ * ก็เพื่อความสุขของตัวเองกันทั้งนั้นแหละ..
+ * แม้
+ * บางคน ที่ 'ทำเพื่อคนอื่น'
+ * แต่สุดท้าย
+ * ก็เพื่อให้ 'ใจตัวเอง' มีความสุข
+ * ‪#‎คนแบบนี้มันน่ากราบ‬
+ * Created by robot on 25/07/15.
+ */
+
+import java.util.Observable;
+import java.util.Observer;
+
+
+class Happiness {
+}
+
+class Man extends Observable implements java.lang.Runnable, Observer {
+    public void run() {
+        while (true) {
+            setChanged();
+            notifyObservers(doForOther());
+        }
+    }
+
+    private Happiness doForOther() {
+        // Do something good.
+        return new Happiness();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        // Finally he got happiness back.
+        Happiness happiness = (Happiness) arg;
+    }
+}
+
+
+public class HappyMan {
+    public static void main(String[] args) {
+        Man man = new Man();
+        // Register himself to be an observer.
+        man.addObserver(man);
+
+        new Thread(man).start();
+    }
+}
